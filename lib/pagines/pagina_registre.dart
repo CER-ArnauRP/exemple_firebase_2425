b9363e3cc8f5967de4e77c07eeac7589a5165341
CER-ArnauRP <a.rodriguez@ceroca.cat>
2025-02-11 17:08:33 +0100

    Pàgina registre iniciada
    Components creats: TextFieldAuth i BotoAuth.

---
+++ lib/pagines/pagina_registre.dart
@@ -0,0 +1,127 @@
+import 'package:exemple_firebase_2425/components/boto_auth.dart';
+import 'package:exemple_firebase_2425/components/textfield_auth.dart';
+import 'package:flutter/material.dart';
+
+class PaginaRegistre extends StatelessWidget {
+  const PaginaRegistre({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController tecEmail = TextEditingController();
+    final TextEditingController tecPassword = TextEditingController();
+    final TextEditingController tecConfPass = TextEditingController();
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 250, 183, 159),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Logo.
+                const Icon(
+                  Icons.fireplace,
+                  size: 120,
+                  color: Color.fromARGB(255, 255, 240, 218),
+                ),
+
+                const SizedBox(height: 25),
+
+                // Frase.
+                const Text(
+                  "Crea un compte nou",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 255, 240, 218),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                const SizedBox(height: 25),
+
+                // Text divisori.
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Color.fromARGB(255, 255, 240, 218),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 4),
+                        child: Text(
+                          "Registra't",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 255, 240, 218)),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 1,
+                          color: Color.fromARGB(255, 255, 240, 218),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                // TextField Email.
+                TextfieldAuth(
+                  controller: tecEmail,
+                  obscureText: false,
+                  hintText: "Escriu el teu email...",
+                ),
+
+                // TextField Password.
+                TextfieldAuth(
+                  controller: tecPassword,
+                  obscureText: true,
+                  hintText: "Tria un password",
+                ),
+
+                // TextField Confirmar password.
+                TextfieldAuth(
+                  controller: tecConfPass,
+                  obscureText: true,
+                  hintText: "Reescriu el password",
+                ),
+
+                const SizedBox(height: 10),
+
+                // No estàs registrat/da?
+                Padding(
+                  padding: const EdgeInsets.only(right: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text("Ja ets membre?"),
+                      const SizedBox(width: 5),
+                      GestureDetector(
+                        child: const Text(
+                          "Fes login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 40, 71, 97),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 10),
+
+                // Botó registra't.
+                BotoAuth(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
