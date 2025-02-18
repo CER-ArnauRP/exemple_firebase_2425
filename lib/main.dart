8002c253a61f1d19a8959dc40502f3dab0ebfc8d
CER-ArnauRP <a.rodriguez@ceroca.cat>
2025-02-18 16:26:05 +0100

    Firebase configurat

--- lib/main.dart
+++ lib/main.dart
@@ -18,3 +18,41 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
+/*
+1) Tenir Node.js instalat.
+    - En acabar, es pot comprobar fent:
+        node -v 
+        npm -v
+
+2) Anar a la web de Firebase i clicar a "Go to console".
+    - Tenir en compte amb quin compte de Google.
+
+3) Des de la consola de Firebase, creem un projecte de 
+  Firebase.
+
+4) Anem al menú Compilación i habilitem: 
+    Authentication i Firestore Database
+
+5) En una cmd, per exemple la del VS Code, fem:
+    npm install -g firebase-tools
+    (això instala Firebase al dispositiu)
+    Pot fer falta fer abans:
+    Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+6) flutter pub global activate flutterfire_cli
+    o dart pub global activate flutterfire_cli
+
+7) flutterfire configure
+    - Seleccionem el projecte de Firebase
+        amb el que el volem vincular.
+    - Deixem seleccionats només els dispositius amb els que 
+        volem que funcioni l'aplicació (en aquest cas android i web).
+    
+    - Si no detecta la comanda flutterfire configure, és que s'ha 
+        d'afegir flutterfire al path (al path de l'usuari de les 
+        variables d'entorn).
+    
+    - La ruta per defecte és:
+        C:\Users\<nom_usuari>\AppData\Local\Pub\Cache\bin
+*/
