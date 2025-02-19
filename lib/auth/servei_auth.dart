9321594c8230189db65c25b8fc4076ff8c387ab2
CER-ArnauRP <a.rodriguez@ceroca.cat>
2025-02-19 17:09:11 +0100

    Registre amb Firestore

--- lib/auth/servei_auth.dart
+++ lib/auth/servei_auth.dart
@@ -1,23 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ServeiAuth {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  
 
   // Fer registre.
   Future<UserCredential> registreAmbEmailIPassword(String email, password) async {
 
-        try{
+    try{
 
-          UserCredential credencialUsuari =
-            await _auth.createUserWithEmailAndPassword(
-              email: email,
-              password: password,
-            );
-            return credencialUsuari;
+      UserCredential credencialUsuari =
+        await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+      );
 
-        } on FirebaseAuthException catch(e) {
-          throw Exception(e.code);
-        }
+      _firestore.collection("Usuaris").doc(credencialUsuari.user!.uid).set({
+        "uid": credencialUsuari.user!.uid,
+        "email": email,
+        "nom": "",
+      });
+
+        return credencialUsuari;
+
+    } on FirebaseAuthException catch(e) {
+      throw Exception(e.code);
+    }
 
     
   }
--- lib/pagines/pagina_registre.dart
+++ lib/pagines/pagina_registre.dart
@@ -6,11 +6,30 @@ import 'package:flutter/material.dart';
 class PaginaRegistre extends StatelessWidget {
   const PaginaRegistre({super.key});
 
-  void ferRegistre() {
+  void ferRegistre(BuildContext context, String email, String password,
+      String confPassword) async {
+    if (password.isEmpty || email.isEmpty) {
+      // Gestionar-ho.
+      return;
+    }
 
-    final ServeiAuth serveiAuth = ServeiAuth();
+    if (password != confPassword) {
+      // Gestió del cas.
+      return;
+    }
 
-    serveiAuth.registreAmbEmailIPassword("email1@email1.com", "123456");
+    try {
+      ServeiAuth().registreAmbEmailIPassword(email, password);
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Error"),
+          content: Text(e.toString()),
+        ),
+      );
+    }
+    
   }
 
   @override
@@ -126,14 +145,13 @@ class PaginaRegistre extends StatelessWidget {
                 // Botó registra't.
                 BotoAuth(
                   text: "Registra't",
-                  onTap: ferRegistre,
+                  onTap: () => ferRegistre(context, tecEmail.text,
+                      tecPassword.text, tecConfPass.text),
                 ),
 
                 BotoAuth(
                   text: "Logout",
-                  onTap: () {
-                    
-                  },
+                  onTap: () {},
                 ),
               ],
             ),
